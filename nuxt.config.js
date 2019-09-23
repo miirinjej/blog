0018fc7b277dfe5e012c2410fa5eee8d2ff6a35f
miirinjej <miirinjej@gmail.com>
2019-09-23 23:52:53 +0500

    Add noscript

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,7 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
     ],
+    noscript: [{ innerHTML: 'Sorry, your browser does not support JavaScript!' }],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
